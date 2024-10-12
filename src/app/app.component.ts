f4c5501f7e69d82ff4648625d8dc120c3573304f
Tomchuk Dima <TomchukD@csiworks.net>
2024-10-12 23:23:35 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'v-15Now';
-}
+export class AppComponent {}
